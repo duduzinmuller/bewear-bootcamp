c732e0e6e1ff0f8c695d1f807c880e3006c44506
duduzinmuller <eduardomuller989@gmail.com>
2025-08-11 16:35:26 -0400

    feat: add product list and product item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@ import Image from "next/image";
 
 import Brands from "./_components/brands";
 import Header from "./_components/header";
+import ProductList from "./_components/product-list";
+import { db } from "./_db";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
-      <div className="space-y-5">
+      <div className="space-y-6 px-5">
         <Image
           src="/frame.png"
           alt="Frame"
@@ -17,7 +24,19 @@ export default function Home() {
           className="h-auto w-full"
         />
         <Brands />
+
+        <ProductList products={products} title="Mais vendidos" />
+        <Image
+          src="/banner.png"
+          alt="Frame"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
       </div>
     </>
   );
-}
+};
+
+export default Home;
