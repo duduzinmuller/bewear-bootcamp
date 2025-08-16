1f13713b368ecbf0ec701677d5a1d25b57b68494
duduzinmuller <eduardomuller989@gmail.com>
2025-08-16 16:19:21 -0400

    feat: add new partner brands and update styling in Brands component

--- app/_components/partner-brands.tsx
+++ app/_components/partner-brands.tsx
@@ -7,7 +7,7 @@ interface PartnerBrandsProps {
 
 const PartnerBrands = ({ brandIcon, name }: PartnerBrandsProps) => {
   return (
-    <div className="flex shrink-0 flex-col items-center gap-2 px-2">
+    <div className="flex shrink-0 flex-col items-center gap-6 p-5 px-15">
       <div className="flex h-8 w-8 items-center justify-center">
         <Image src={brandIcon} alt={name} width={24} height={24} />
       </div>
